094f09f6411d7f00159b4010391b22fe07145714
Griff <griffinmickdonald@gmail.com>
2025-01-19 09:24:26 -0600

    Fixed pig pathfinding. Now we can use the pathfinding component in all types of entities, it's pretty cool. Next step is to refactor the player scene to use our components

--- scripts/pig.gd
+++ scripts/pig.gd
@@ -1,7 +1,17 @@
 extends CharacterBody2D
 
 @export var pathfind_target: Node2D
+var last_target_position: Vector2 
 
+func _ready() -> void:
+	update_pathfind_target_position()
 
+func update_pathfind_target_position():
+	get_node("PathfindComponent").target_position = pathfind_target.global_position
+	print("Setting target position for pig PathfindComponent: " + str(pathfind_target.global_position))
+	
+func _process(delta: float) -> void:
+	update_pathfind_target_position()
+	
 func _physics_process(delta: float) -> void:
 	pass
