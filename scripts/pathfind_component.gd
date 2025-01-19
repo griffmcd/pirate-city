26c07a072d0a737f0745f15d1f4a236e3580295d
Griff <griffinmickdonald@gmail.com>
2025-01-19 06:24:16 -0600

    First refactor for components. Used pig scene as the initial tester. Pathfinding isn't quite working--thats the next step. Once that's in place, we have most of the pieces to rework the player class using composition. Once that's in place, we are ready to start with the ships and island generation, I think

--- scripts/pathfind_component.gd
+++ scripts/pathfind_component.gd
@@ -7,42 +7,47 @@ class_name PathfindComponent
 @export var target: Node2D
 @export var debug_draw_enabled = true 
 @export var movement_speed = 4.0
-@export var navigation_agent: NavigationAgent2D
+@onready var navigation_agent: NavigationAgent2D = get_node("NavAgent2D")
+var movement_delta: float 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	navigation_agent = $NavigationAgent2D
 	var agent: RID = navigation_agent.get_rid()
 	NavigationServer2D.agent_set_avoidance_enabled(agent, true)
-	NavigationServer2D.agent_set_avoidance_callback(agent, Callable(self, "_avoidance_done"))
 	navigation_agent.velocity_computed.connect(Callable(_on_velocity_computed))
-	navigation_agent.target_position = Vector2.ZERO
+	set_movement_target(Vector2(100, 100))
 	
 func set_movement_target(movement_target: Vector2):
 	navigation_agent.set_target_position(movement_target)
 	
 func _physics_process(delta: float) -> void:
 	# do not query when the map has never synchronized and is empty 
-	if NavigationServer2D.map_get_iteration_id(navigation_agent.get_navigation_map()):
-		print("1")
+	if NavigationServer2D.map_get_iteration_id(navigation_agent.get_navigation_map()) == 0:
+		print("f")
 		return
 	if navigation_agent.is_navigation_finished():
-		print("2")
+		print("G")
 		return
-	print('f')
+	movement_delta = movement_speed * delta 
+	print("movement_delta: " + str(movement_delta))
 	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
-	var new_velocity: Vector2 = global_position.direction_to(next_path_position) * movement_speed
+	print("Next path position: " + str(next_path_position))
+	print(global_position)
+	var new_velocity: Vector2 = global_position.direction_to(next_path_position) * movement_delta 
+	print("New velocity: " + str(new_velocity))
 	if navigation_agent.avoidance_enabled:
-		navigation_agent.set_velocity_forced(new_velocity)
+		navigation_agent.set_velocity(new_velocity)
+		print("Avoidance enabled, setting velocity: " + str(new_velocity))
 	else:
+		print("Entering on_velocity_computed with velocity: " + str(new_velocity) + " and delta: " + str(delta))
 		_on_velocity_computed(new_velocity, delta)
 		
 func _on_velocity_computed(safe_velocity: Vector2, delta: float): 
 	velocity_component.accelerate(safe_velocity, delta)
 	velocity_component.move(character_body)
 	var final_position = navigation_agent.get_final_position()
-	print(final_position)
+	print("Final position: " + str(final_position))
 	character_body.move_and_slide()
 	
 	
