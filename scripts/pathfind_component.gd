f2314c4be7a4db50c020654383b46569c7d679c4
Griff <griffinmickdonald@gmail.com>
2025-01-19 14:13:03 -0600

    further progress on player refactor, some tweaks to pathfinding
    see commit summary

--- scripts/pathfind_component.gd
+++ scripts/pathfind_component.gd
@@ -5,9 +5,10 @@ class_name PathfindComponent
 @export var animation_component: AnimationComponent
 @export var character_body: CharacterBody2D
 @export var target_position: Vector2
+@export var collision_radius: int = 0
 var last_target_position: Vector2 = Vector2.ZERO
-@export var path_desired_distance: float = 100.0
-@export var target_desired_distance: float = 2.0
+@export var path_desired_distance: float = 20.0
+@export var target_desired_distance: float = 20.0
 @onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
 
 # Called when the node enters the scene tree for the first time.
@@ -24,21 +25,30 @@ func actor_setup() -> void:
 	set_movement_target(target_position)
 	last_target_position = target_position 
 	var velocity = global_position.direction_to(target_position) * movement_speed
-	animation_component.update_animation_based_on_velocity(velocity)
+	animation_component.update_animation_based_on_pathfinding_velocity(velocity)
 	
 func set_movement_target(movement_target: Vector2):
 	navigation_agent.set_target_position(movement_target)
+	
+func is_within_collision_radius() -> bool:
+	var offset = Vector2(collision_radius, 0).rotated(navigation_agent.radius * PI / 2)
+	return target_position.distance_to(global_position + offset) <= collision_radius
 	
 func _process(delta: float) -> void:
 	if (target_position != last_target_position):
 		set_movement_target(target_position)
 	
+	
 func _physics_process(delta: float) -> void:
 	if navigation_agent.is_navigation_finished():
+		velocity_component.velocity = Vector2.ZERO
+		return
+	if navigation_agent.is_target_reached() or is_within_collision_radius():
+		velocity_component.velocity = Vector2.ZERO
 		return
 	var current_agent_position: Vector2 = global_position 
 	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
 	var velocity = current_agent_position.direction_to(next_path_position) * movement_speed
-	animation_component.update_animation_based_on_velocity(velocity)
+	animation_component.update_animation_based_on_pathfinding_velocity(velocity)
 	velocity_component.accelerate(velocity, delta)
 	velocity_component.move(character_body)
--- scripts/velocity_component.gd
+++ scripts/velocity_component.gd
@@ -6,6 +6,7 @@ signal at_max_speed
 # TODO: Should we keep track of old velocity and signal changes from here?
 @export var max_speed = 100.0
 @export var acceleration = 1000.0
+@export var deceleration = 100.0
 @export var velocity: Vector2
 @export var friction = 500.0
 @export var debug_mode = true 
@@ -22,14 +23,15 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	decelerate(delta)
 	
 func accelerate(new_velocity, delta):
 	velocity = velocity.move_toward(new_velocity * max_speed, acceleration * delta)
 	
 func decelerate(delta):
-	velocity = velocity.move_toward(Vector2.ZERO, acceleration * delta)
+	velocity = velocity.move_toward(Vector2.ZERO, deceleration * delta)
 	
 func move(body: CharacterBody2D):
 	body.velocity = velocity
+	body.move_and_slide()
 	
