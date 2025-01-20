8e4b4fefc94dda3c34dba8bfc2768165d8976b6f
Griff <griffinmickdonald@gmail.com>
2025-01-19 18:14:20 -0600

    initial interior work
    Initial work testing moving inside and outside of a house. Will need a
    little more refactoring--currently we load and unload the main scene,
    but we don't actually want to do that

--- scripts/door.gd
+++ scripts/door.gd
@@ -1,5 +1,6 @@
 extends Area2D
-
+@export var interior_scene_path: String
+var player_world_coordinates: Vector2 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,6 +11,10 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-
+func _change_scene():
+	get_tree().change_scene_to_file(interior_scene_path)
+	pass
 func _on_body_entered(body: Node2D) -> void:
+	player_world_coordinates = body.global_position
+	_change_scene()
 	print("Entering door")
